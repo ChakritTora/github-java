8fa1c2bfe3533f61a09bec591f572e0b5f0b99ba
ChakritTora <chakrit.tora@gmail.com>
2016-03-02 02:02:02 +0700

    Add example 2-3

---
+++ JavaExample2_3/src/javaexample2_3/JavaExample2_3.java
@@ -0,0 +1,17 @@
+package javaexample2_3;
+
+/**
+ *
+ * @author Mr.Chakrit Tokuhara
+ */
+public class JavaExample2_3 {
+
+    public static void main(String[] args) {
+        System.out.println("6 + 5 = " + (6 + 5));
+        System.out.println("16 - 7 = " + (16 - 7));
+        System.out.println("14 * 5 = " + (14 * 5));
+        System.out.println("29 / 4 = " + (29 / 4));
+        System.out.println("40 % 9 = " + (40 % 9));
+    }
+    
+}
