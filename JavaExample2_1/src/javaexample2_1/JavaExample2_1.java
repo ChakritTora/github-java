f87cf0fdc1eee8dae7d25356d8e091cfc2862581
ChakritTora <chakrit.tora@gmail.com>
2016-03-02 01:50:51 +0700

    Add example 2-1

---
+++ JavaExample2_1/src/javaexample2_1/JavaExample2_1.java
@@ -0,0 +1,14 @@
+package javaexample2_1;
+
+/**
+ *
+ * @author Mr. Chakrit Tokuhara
+ */
+
+public class JavaExample2_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java programming");  // แสดงข้อความ Welcome to Java Programming
+    }
+    
+}
