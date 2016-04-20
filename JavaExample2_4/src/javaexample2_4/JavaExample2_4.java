e9397dacf9a4af9d628dc566fae6bab7d5c3fa9d
ChakritTora <chakrit.tora@gmail.com>
2016-04-20 09:16:22 +0700

    update new java file

---
+++ JavaExample2_4/src/javaexample2_4/JavaExample2_4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample2_4;
+
+/**
+ *
+ * @author Mr. Chakrit Tokuhara
+ */
+public class JavaExample2_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("5.4 + 2.9 = " + (5.4 + 2.9));
+        System.out.println("3.0 + 4.4 = " + (3.0 + 4.4));
+        System.out.println("12.0 - 4.5 = " + (12.0 - 4.5));
+        System.out.println("9.4 - 6.7 = " + (9.4 - 6.7));
+        System.out.println("2.5 * 4.0 = " + (2.5 * 4.0));
+        System.out.println("4.5 * 3.5 = " + (4.5 * 3.5));
+        System.out.println("18.0 / 4.0 = " + (18.0 / 4.0));
+        System.out.println("12.5 / 2.5 = " + (12.5 / 2.5));
+        System.out.println("10.0 % 2.7 = " + (10.0 % 2.7));
+        System.out.println("9.8 % 1.1 = " + (9.8 % 1.1));
+        
+    }
+    
+}
