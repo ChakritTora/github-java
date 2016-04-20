0c46815e33cf0d3ac753620565251c880c4a4e5f
ChakritTora <chakrit.tora@gmail.com>
2016-04-20 12:52:02 +0700

    add new java file

---
+++ JavaExample2_8/src/javaexample2_8/JavaExample2_8.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample2_8;
+
+/**
+ *
+ * @author Mr.Chakrit Tokuhara
+ */
+public class JavaExample2_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("4 + 5 * 6 = " + (4 + 5 * 6));
+        System.out.println("2 * 4 + 6 = " + (2 * 4 + 6));
+        System.out.println("9.0 / 2.5 - 1.5 = " + (9.0 / 2.5 - 1.5));
+        System.out.println("3.0 * 4.0 / 5.0 = " + (3.0 * 4.0 / 5.0));
+    }
+    
+}
