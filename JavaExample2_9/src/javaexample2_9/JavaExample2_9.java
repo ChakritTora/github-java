a53834014f6fd1cf8cd36307006b6167025e8c75
ChakritTora <chakrit.tora@gmail.com>
2016-04-20 13:23:39 +0700

    update new java file

---
+++ JavaExample2_9/src/javaexample2_9/JavaExample2_9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample2_9;
+
+/**
+ *
+ * @author Mr.Chakrit Tokuhara
+ */
+public class JavaExample2_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("(int)(7.9) = " + (int)(7.9));
+        System.out.println("(int)(3.4) = " + (int)(3.4));
+        System.out.println("(double)(9) = " + (double)(9));
+        System.out.println("(double)(3 + 4) = " + (double)(3 + 4));
+        System.out.println("(double)(15) / 2 = " + ((double)(15)/2));
+        System.out.println("(double)(15 / 2) = " + (double)(15/2));
+    }
+    
+}
