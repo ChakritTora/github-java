cc0f324fd302fdce4d18c8e3eb4581621d1db8a5
ChakritTora <chakrit.tora@gmail.com>
2016-04-20 13:32:26 +0700

    update new java files

---
+++ JavaExample2_11/src/javaexample2_11/JavaExample2_11.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample2_11;
+
+/**
+ *
+ * @author Mr.Chakrit Tokuhara
+ */
+public class JavaExample2_11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        final double CENTIMETERS_PER_INCH = 2.54;
+        final int NUM_STUDENT = 50;
+        
+    }
+    
+}
---
+++ JavaExample2_13/src/javaexample2_13/JavaExample2_13.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaexample2_13;
+
+/**
+ *
+ * @author Mr.Chakrit Tokuhara
+ */
+public class JavaExample2_13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1, num2;
+        double num3;
+        char char1;
+        String text1;
+        
+        num1 = 15;
+        num2 = 40;
+        num3 = 5.7;
+        char1 = 'S';
+        text1 = "Hello world!";
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(char1);
+        System.out.println(text1);
+    }
+    
+}
